b66a53a1f7efc85229dfe25d6101e8733b6dac52
Jacinda Moore <alynna.kasmira@gmail.com>
2014-02-22 17:41:41 -0500

    partially add audio recording capabilities

--- BirdSong/src/sound/birdsong/ListenActivity.java
+++ BirdSong/src/sound/birdsong/ListenActivity.java
@@ -1,5 +1,6 @@
 package sound.birdsong;
 
+import android.media.AudioRecord;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -10,12 +11,19 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 
-public class ListenActivity extends Activity {
+public class ListenActivity extends Activity
+{
+	private AudioRecord _recorder;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_listen);
+		
+		_recorder = new AudioRecord();
+		_recorder.startRecording();
+		
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
@@ -24,29 +32,36 @@ public class ListenActivity extends Activity {
 	 * Set up the {@link android.app.ActionBar}, if the API is available.
 	 */
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+	private void setupActionBar()
+	{
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+		{
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
 	}
 
 	public void endListen(View view)
 	{
-    	Intent intent = new Intent(this, AnalysisActivity.class);
-    	// TODO: send the sound
-    	startActivity(intent);
+		_recorder.stop();
+		// TODO: pull the sound data out of the recorder
+		Intent intent = new Intent(this, AnalysisActivity.class);
+		// TODO: send the sound
+		startActivity(intent);
 	}
-	
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.listen, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
 		case android.R.id.home:
 			// This ID represents the Home or Up button. In the case of this
 			// activity, the Up button is shown. Use NavUtils to allow users
